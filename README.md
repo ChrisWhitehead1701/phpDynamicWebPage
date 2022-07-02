c29ae4171c64010f5329f34075e8a00bc165db10
GeekGamerDad1701 <106108581+GeekGamerDad1701@users.noreply.github.com>
2022-07-01 21:37:05 -0500

    first upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# PHPDynamicWebSite
+ UdemyProject
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dynamic Website</title>
+    <link href="styles.css" rel="stylesheet">
+
+</head>
+
+<body id="final-example">
+
+    <div class="wrapper">
+
+        <div id="banner">
+            <a href="/" title="Return to Home">
+                <img src="/phpDynamicWebPage/img/banner.png" alt="Chris's Fine Dining">
+            </a>
+
+            <div id="nav">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="team">Team</a></li>
+                    <li><a href="menu.php">Menu</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+
+            </div>
+            <!--nav-->
+
+            <div class="content">
+
+                <div id="footer" class="cf">
+
+                    <div class="column three">
+                        <strong>Phone</strong>
+                        867-5309
+
+                    </div>
+                    <!--column three-->
+
+                    <div class="column three">
+                        <strong>Location</strong>
+                        1234 upInSmoke Blvd. Funland, NW
+
+                    </div>
+                    <!--column three-->
+
+                    <div class="column three last">
+                        <strong>Hours</strong>
+                        <em>Tuesday - Thursday</em><br>
+                        1:00pm - 9:00pm<br><br>
+
+                        <strong>Hours</strong>
+                        <em>Friday - Saterday</em><br>
+                        4:00pm - 11:00pm<br><br>
+
+                        <strong>Hours</strong>
+                        <em>Sunday - Monday</em><br>
+                        closed<br><br>
+                    </div>
+                    <!--column three last-->
+
+
+                </div>
+                <!--footer-->
+                <small>&copy;2022 Chris Whitehead</small>
+
+            </div>
+            <!--content-->
+
+
+        </div>
+        <!--banner-->
+
+    </div>
+    <!--wrapper-->
+
+    <div class="copyright-info">
+        <h3>My Own Build</h3>
+    </div>
+    <!--copyright-info-->
+
+</body>
+
+</html>
